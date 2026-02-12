90ce4b93d1d37d04b20d022720af027792aeb32c
mohamedramadan <mogdwd@gmail.com>
2026-02-12 18:53:24 +0200

    chore: prepare for v2 rewrite

--- components/spinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function Spinner() {
-  return (
-    <div className="flex items-center justify-center">
-      <div className="animate-spin rounded-full border-4 border-light-primary border-t-primary p-2"></div>
-    </div>
-  );
-}
--- utils/hijriDate.tsx
+++
@@ -1,6 +0,0 @@
-const hijriDate = new Intl.DateTimeFormat('ar-TN-u-ca-islamic', {
-  day: 'numeric',
-  month: 'long',
-  year: 'numeric',
-}).format(Date.now())
-export { hijriDate }
