84b8dc2b2b6d0b2b3f77d161a216ef652d9976d4
mohamedramadan <mogdwd@gmail.com>
2026-03-08 23:10:58 +0200

    feat: create ZikrList component to display a grid of ZikrCards with dynamic rendering based on detection data

---
+++ packages/ui/src/layout/ZikrList.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { cn } from "@workspace/lib/utils";
+import { type Detections } from "@workspace/model/types";
+import ZikrCard from "./ZikrCard";
+
+export default function ZikrList({
+  list,
+  LinkComponent = "a",
+}: {
+  list: Detections;
+  LinkComponent?: React.ElementType;
+}) {
+  return (
+    <div className="grid grid-cols-2 gap-2">
+      {Object.entries(list)
+        .filter(([_, zikr]) => zikr.render)
+        .map(([id, zikr]) => {
+          return (
+            <LinkComponent key={id} href={`/zikr/${id}`}>
+              <ZikrCard
+                className={cn(
+                  zikr.count % 2 === 0 ? "shimmer-even" : "shimmer-odd",
+                )}
+              >
+                <ZikrCard.Count>{zikr.count}</ZikrCard.Count>
+                <ZikrCard.Header>{zikr.label}</ZikrCard.Header>
+              </ZikrCard>
+            </LinkComponent>
+          );
+        })}
+    </div>
+  );
+}
